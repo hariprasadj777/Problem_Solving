28e087a5925a234850894da45972c33d10814bad
hariprasadj777 <107306614+hariprasadj777@users.noreply.github.com>
2022-07-05 08:28:39 +0530

    Update Leap_year.py

--- Leap_year.py
+++ Leap_year.py
@@ -1,7 +1,7 @@
 #To find whether a given year is a leap year
 year =int(input("Enter the year:"))
 if (year % 400 == 0) and (year % 100 == 0):
-    print("{0} is a leap year".format(year))
+    print("Is a leap year")
 elif (year % 4 ==0) and (year % 100 != 0):
     print("Is a leap year")
 else:
